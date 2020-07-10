81f07e98326fdc5523f09709c4e0f492f33e4351
wambergo <wamberg@protonmail.com>
2020-07-10 11:35:06 +0200

    cost trajectory

--- garbage_plots.py
+++ garbage_plots.py
@@ -1,17 +1,27 @@
 import matplotlib.pyplot as plt
+from main import *
 
-plt.figure(figsize=(16,16))
+plt.figure(figsize=(16, 16))
 ctrs = np.zeros((200, 10))
-for i in range(0,200):
+for i in range(0, 200):
     mpc.update_market()
-    ctrs[i,:] = mpc.ctr
+    ctrs[i, :] = mpc.ctr
     
-plt.plot(ctrs[:,])
+plt.plot(ctrs[:, ])
 plt.show()
 
-np.mean(ctrs[:,])
+np.mean(ctrs[:, ])
 
 
 ########
 
-plt.plot(past_bids[0, :], past_costs[0, :],'*')
+plt.plot(past_bids[0, :], past_costs[0, :], '*')
+
+
+#######
+# our cost and the target budget
+cumsum_cost = np.cumsum(running_total_cost)
+
+plt.plot(cumsum_cost)
+plt.plot(y_target)
+plt.show()
